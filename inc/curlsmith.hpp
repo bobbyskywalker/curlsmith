35a994ecc28508a5768f8c1babd8d7c329dd87ea
bobbyskywalker <aleksander.garbacz@gmail.com>
2025-11-17 19:14:54 +0100

    project cmake init

---
+++ inc/curlsmith.hpp
@@ -0,0 +1,3 @@
+#pragma once
+
+#define CURLSMITH_VERSION "0.1"
---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../inc/curlsmith.hpp"
+
+int main(int ac, char **av) {
+    if (ac != 1) {
+        std::cout << "curlsmith: invalid number of arguments" << std::endl;
+        std::exit(1);
+    }
+
+
+    return (0);
+}
